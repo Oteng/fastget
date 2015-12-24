3ed48decb277f003a28959f52db1de2a807bddc7
oteng <otengkwaku@gmail.com>
2015-12-24 23:19:22 +0000

    added get_block_size function

---
+++ downloader/download.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+
+class Downloader:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_block_size(elapsed_time, bytes):
+        new_min = max(bytes / 2.0, 1.0)
+        new_max = min(max(bytes * 2.0, 1.0), 4194304)  # Do not surpass 4 MB
+        if elapsed_time < 0.001:
+            return int(new_max)
+        rate = bytes / elapsed_time
+        if rate > new_max:
+            return int(new_max)
+        if rate < new_min:
+            return int(new_min)
+        return int(rate)
