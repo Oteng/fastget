17f90feb614164cd7cb7978fbeca94ee8d4d4caf
oteng <otengkwaku@gmail.com>
2015-12-24 23:54:39 +0000

    change get_buffer to set_buffer and also change how it is implemented

--- downloader/download.py
+++ downloader/download.py
@@ -3,17 +3,16 @@ from __future__ import unicode_literals
 
 class Downloader:
     def __init__(self):
-        pass
+        self.buffer = None
 
-    @staticmethod
-    def get_block(elapsed_time, bytes):
+    def set_buffer(self, elapsed_time=None, bytes=None):
         new_min = max(bytes / 2.0, 1.0)
         new_max = min(max(bytes * 2.0, 1.0), 4194304)  # Do not surpass 4 MB
         if elapsed_time < 0.001:
-            return int(new_max)
+            self.buffer = int(new_max)
         rate = bytes / elapsed_time
         if rate > new_max:
-            return int(new_max)
+            self.buffer = int(new_max)
         if rate < new_min:
-            return int(new_min)
-        return int(rate)
+            self.buffer = int(new_min)
+        self.buffer = int(rate)
