00d824a4f41ea5de3dcf9aab5ea26a236346d9e8
oteng <otengkwaku@gmail.com>
2015-12-25 01:49:00 +0000

    fixed errors and added comments

--- downloader/http.py
+++ downloader/http.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 import requests
 import os
-import Downloader
+from download import Downloader
 import time
 
 """
@@ -11,7 +11,15 @@ and writing then to disk
 
 
 class HTTP(Downloader):
+    """
+    this class represent a single instance of the downloading of a file.
+    multiple instances of this class running multiple threads that is how it works
+    It does this by downloading a particular segment of a file specified by the content-range
+    header
+    """
+
     def __init__(self, file_info):
+        Downloader.__init__(self)
         self.start = None
         self.end = None
         self.file_info = file_info
@@ -20,15 +28,16 @@ class HTTP(Downloader):
         self.downloaded_byte = 0
 
     def download(self):
-        data_handle = requests.get(self.file_info.data_url)
+        # TODO: check status code and report reason for aborting download to controller
+        data_handle = requests.get(self.file_info["data_url"], headers=self.file_info["header"], stream=True)
         data = data_handle.raw
 
         before = time.time()
         while True:
             # get a block of data from the server
-            data_block = data.read(self.buffer if self.buffer is not None else 1024)
+            data_block = data.read(self.buffer if self.buffer is not None else min((self.end - self.start), 1024))
             self.downloaded_byte += len(data_block)
-
+            print self.buffer
             # break out of loop when download is done
             if len(data_block) == 0:
                 self.file.close()
@@ -37,7 +46,7 @@ class HTTP(Downloader):
             # open file just in time
             if self.file is None:
                 try:
-                    self.file = open(self.file_info.file_name, "wb")
+                    self.file = open(self.file_info["file_name"], "wb")
                     assert self.file is not None
                     self.file.seek(self.start, os.SEEK_CUR)
                 except (IOError, OSError) as err:
@@ -60,7 +69,6 @@ class HTTP(Downloader):
             before = after
 
     def cal_offset(self):
-        _range = self.file_info.range.split("-")
+        _range = self.file_info["range"].split("-")
         self.start = int(_range[0])
         self.end = int(_range[1])
-
